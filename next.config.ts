ecb25553c7cdd7deade010b834501ded5ff24779
Thio Saputra <kambinghirang2@gmail.com>
2025-10-28 14:10:21 +0000

    fix the error again? do re code

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,16 @@ import type {NextConfig} from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-// This will be set by the GitHub Actions workflow
+// This will be set by the GitHub Actions workflow.
 const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
-  // Set the basePath and assetPrefix for GitHub Pages
+  // Configure basePath and assetPrefix for GitHub Pages deployment.
   basePath: isProd ? `/${repoName}` : '',
   assetPrefix: isProd ? `/${repoName}/` : '',
+  // Add a trailing slash to all paths.
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
