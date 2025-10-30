111cc5bad436ad3265c6ae17dfbfc24d83216caa
Thio Saputra <kambinghirang2@gmail.com>
2025-10-30 12:12:54 +0000

    perbaiki lagi, aku cek di https://flessan.github.io tampilan tidak sesua

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@ import type {NextConfig} from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-// This will be set by the GitHub Actions workflow.
-const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
+// The base path is read from an environment variable set during the build process.
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
   // Configure basePath and assetPrefix for GitHub Pages deployment.
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? basePath : '',
+  assetPrefix: isProd ? `${basePath}/` : '',
   // Add a trailing slash to all paths.
   trailingSlash: true,
   typescript: {
